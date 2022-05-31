7d912fe15e6ab4aa07b5be7c8ee4ae61a3866add
jawsberrebi <berjaws@gmail.com>
2022-05-31 20:38:56 +0200

    Problèmes Maven pour Windows résolus

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.ricochet_robot {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example.ricochet_robot to javafx.fxml;
